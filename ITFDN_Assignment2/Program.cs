c212b04a6d5a98bc16d0727df5cfa26426d8f802
David Clayton <clayton@naturalselectionmusic.com>
2017-07-13 18:54:52 -0700

    Committing Code

---
+++ ITFDN_Assignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITFDN_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "1024";
+            int answer = 0;
+            int zeroPosition = 48;
+            for(int i = 0; i<input.Length; i++)
+            {
+                answer = answer + (input[i] - zeroPosition) * (int)Math.Pow(10,(double)(input.Length-i-1));
+            }
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ITFDN120_Assignment2
+https://canvas.uw.edu/courses/1166106/assignments
+
+Write the code to convert the string "1024" to an integer
+
+For base 10, the number needs to be multiplied by 10 to the power of its position
