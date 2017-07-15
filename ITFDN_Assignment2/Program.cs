41c5baccfb94b0d53557e76eaab8b6d210f525fc
David Clayton <david@davidclayton.me>
2017-07-15 09:31:41 -0700

    Added comments for intention

--- ITFDN_Assignment2/Program.cs
+++ ITFDN_Assignment2/Program.cs
@@ -12,9 +12,12 @@ namespace ITFDN_Assignment2
         {
             string input = "1024";
             int answer = 0;
+            //Set the ASCII position of 0
             int zeroPosition = 48;
             for(int i = 0; i<input.Length; i++)
             {
+                //Use the character's ASCII position to determine the number it is and
+                //use the position in the string to determine the power of 10
                 answer = answer + (input[i] - zeroPosition) * (int)Math.Pow(10,(double)(input.Length-i-1));
             }
 
